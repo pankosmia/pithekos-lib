e354411f5045d922eeae8dde2d3733a2d0761662
Mark Howe <mvahowe@gmail.com>
2025-04-29 21:02:16 +0200

    Header without grid

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -62,121 +62,108 @@ function Header({titleKey, widget, currentId}) {
         [debugRef.current]
     )
     const currentUrl = menuItems.filter(i => i.id === currentId).length === 1 ? menuItems.filter(i => i.id === currentId)[0].url : "";
-    return <div sx={{flexGrow: 1}}>
+    return <Box display="flex-start" sx={{flexGrow: 1, m:0, p:0}}>
         <AppBar position="static">
             <Toolbar sx={{backgroundColor: "#441650"}}>
-                <Grid2 container direction="row"
-                       justifyContent="flex-end"
-                       alignItems="center"
-                       >
-                    <Grid2 container size={{xs: 1}} justifyContent="flex-start" alignItems="left">
-                        <IconButton onClick={e => setDrawerIsOpen(true)}>
-                            <MenuIcon sx={{color: "#FFF"}}/>
-                        </IconButton>
-                        <Drawer
-                            open={drawerIsOpen} onClose={() => setDrawerIsOpen(false)}
-                        >
-                            <Box sx={{width: 250}} role="presentation" onClick={() => setDrawerIsOpen(false)}>
-                                <List>
-                                    {
-                                        menuItems
-                                            .map(
-                                                (mi, n) => mi.id === currentId ?
-                                                    <ListItem key={n} disablePadding>
-                                                        <ListItemButton selected={true}>
-                                                            <ListItemText
-                                                                primary={doI18n(`pages:${mi.id}:title`, i18nRef.current)}/>
-                                                        </ListItemButton>
-                                                    </ListItem> :
-                                                    <ListItem key={n} disablePadding>
-                                                        <ListItemButton
-                                                            disabled={mi.requires.net && !enabledRef.current}
-                                                            onClick={() => {
-                                                                window.location.href = mi.url
-                                                            }}
-                                                        >
-                                                            <ListItemText
-                                                                primary={doI18n(`pages:${mi.id}:title`, i18nRef.current)}/>
-                                                        </ListItemButton>
-                                                    </ListItem>
-                                            )
-                                    }
-                                </List>
-                            </Box>
-                        </Drawer>
-                    </Grid2>
-                    <Grid2 container size={{xs: 5, md: 4, lg: 3}} justifyContent="flex-start">
-                        {titleKey && titleKey.length > 0 &&
-                            <Typography variant="h6">{doI18n(titleKey, i18nRef.current)}</Typography>}
-                    </Grid2>
-                    <Grid2 container size={{xs: 2, md: 4, lg: 5}} justifyContent="flex-start">
-                        {widget}
-                    </Grid2>
-                    <Grid2 container size={{xs: 4, md: 3, lg: 3}} justifyContent="flex-end">
-                        {
-                            <>
-                                <IconButton onClick={e => setAuthAnchorEl(e.currentTarget)}
-                                            disabled={!enabledRef.current}>
-                                    {enabledRef.current ? <Cloud sx={{color: "#FFF"}}/> :
-                                        <CloudOff sx={{color: "#AAA"}}/>}
-                                </IconButton>
-                                <Menu
-                                    id="auth-menu"
-                                    anchorEl={authAnchorEl}
-                                    open={authOpen}
-                                    onClose={() => setAuthAnchorEl(null)}
-                                    anchorOrigin={{
-                                        vertical: 'top',
-                                        horizontal: 'left',
-                                    }}
-                                    transformOrigin={{
-                                        vertical: 'top',
-                                        horizontal: 'left',
-                                    }}
-                                >{
-                                    Object.entries(authRef.current)
+                <Box>
+                    <IconButton onClick={e => setDrawerIsOpen(true)}>
+                        <MenuIcon sx={{color: "#FFF"}}/>
+                    </IconButton>
+                    <Drawer
+                        open={drawerIsOpen} onClose={() => setDrawerIsOpen(false)}
+                    >
+                        <Box sx={{width: 250}} role="presentation" onClick={() => setDrawerIsOpen(false)}>
+                            <List>
+                                {
+                                    menuItems
                                         .map(
-                                            (mi, n) => <MenuItem
-                                                key={n}
-                                                onClick={() => {
-                                                    window.location.href = mi[1].isActive ? `/gitea/logout/${mi[0]}/` : `/gitea/login/${mi[0]}/${currentUrl}`;
-                                                }}
-                                            >{
-                                                `${mi[0]} ${mi[1].isActive ? "✓" : "❌"}`
-                                            }</MenuItem>
+                                            (mi, n) => mi.id === currentId ?
+                                                <ListItem key={n} disablePadding>
+                                                    <ListItemButton selected={true}>
+                                                        <ListItemText
+                                                            primary={doI18n(`pages:${mi.id}:title`, i18nRef.current)}/>
+                                                    </ListItemButton>
+                                                </ListItem> :
+                                                <ListItem key={n} disablePadding>
+                                                    <ListItemButton
+                                                        disabled={mi.requires.net && !enabledRef.current}
+                                                        onClick={() => {
+                                                            window.location.href = mi.url
+                                                        }}
+                                                    >
+                                                        <ListItemText
+                                                            primary={doI18n(`pages:${mi.id}:title`, i18nRef.current)}/>
+                                                    </ListItemButton>
+                                                </ListItem>
                                         )
                                 }
-                                </Menu>
-                            </>
-                        }
-                        {
-                            <InternetSwitch
-                                internet={enabledRef.current}
-                                setInternet={
-                                    () => postEmptyJson(enabledRef.current ? '/net/disable' : '/net/enable', debugRef.current)
+                            </List>
+                        </Box>
+                    </Drawer>
+                </Box>
+                {titleKey && titleKey.length > 0 &&
+                    <Typography variant="h6">{doI18n(titleKey, i18nRef.current)}</Typography>}
+                <Box sx={{flexGrow: 1}}>
+                    {widget}
+                </Box>
+                <Box>
+                    <IconButton onClick={e => setAuthAnchorEl(e.currentTarget)}
+                                disabled={!enabledRef.current}>
+                        {enabledRef.current ? <Cloud sx={{color: "#FFF"}}/> :
+                            <CloudOff sx={{color: "#AAA"}}/>}
+                    </IconButton>
+                    <Menu
+                        id="auth-menu"
+                        anchorEl={authAnchorEl}
+                        open={authOpen}
+                        onClose={() => setAuthAnchorEl(null)}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                    >{
+                        Object.entries(authRef.current)
+                            .map(
+                                (mi, n) => <MenuItem
+                                    key={n}
+                                    onClick={() => {
+                                        window.location.href = mi[1].isActive ? `/gitea/logout/${mi[0]}/` : `/gitea/login/${mi[0]}/${currentUrl}`;
+                                    }}
+                                >{
+                                    `${mi[0]} ${mi[1].isActive ? "✓" : "❌"}`
+                                }</MenuItem>
+                            )
+                    }
+                    </Menu>
+                </Box>
+                <InternetSwitch
+                    internet={enabledRef.current}
+                    setInternet={
+                        () => postEmptyJson(enabledRef.current ? '/net/disable' : '/net/enable', debugRef.current)
+                    }
+                />
+                <IconButton>
+                    {
+                        <SettingsIcon
+                            color="inherit"
+                            aria-label="settings"
+                            onClick={
+                                () => {
+                                    window.location.href = "/clients/settings"
                                 }
-                            />
-                        }
-                        <IconButton>
-                            {
-                                <SettingsIcon
-                                    color="inherit"
-                                    aria-label="settings"
-                                    onClick={
-                                        () => {
-                                            window.location.href = "/clients/settings"
-                                        }
-                                    }
-                                    disabled={currentId.includes("settings")}
-                                    sx={{ml: 2, color: currentId.includes("settings") ? "#AAA" : "#FFF"}}
-                                />
                             }
-                        </IconButton>
-                    </Grid2>
-                </Grid2>
+                            disabled={currentId.includes("settings")}
+                            sx={{ml: 2, color: currentId.includes("settings") ? "#AAA" : "#FFF"}}
+                        />
+                    }
+                </IconButton>
             </Toolbar>
         </AppBar>
-    </div>
+    </Box>
 }
 
 export default Header;
