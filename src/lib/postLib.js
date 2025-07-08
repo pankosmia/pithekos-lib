b7196a7bec78d6666ea33c35f777fec0209e7dea
Mark Howe <mvahowe@gmail.com>
2025-07-08 18:00:20 +0200

    postText

--- src/lib/postLib.js
+++ src/lib/postLib.js
@@ -81,4 +81,48 @@ async function postJson(url, body, debug=false, contentType = "application/json"
     }
 }
 
-export {postEmptyJson, postJson};
+async function postText(url, body, debug=false, contentType = "text/plain") {
+    try {
+        const response = await fetch(
+            url,
+            {
+                method: "POST",
+                headers: {"Content-Type": contentType},
+                body});
+        if (!response.ok) {
+            const result = {
+                url,
+                ok: false,
+                status: response.status,
+                error: await response.text()
+            };
+            if (debug) {
+                console.log("postText", result);
+            }
+            return result;
+        }
+        const result = {
+            url,
+            ok: true,
+            status: response.status,
+            text: await response.text()
+        };
+        if (debug) {
+            console.log("postText", result);
+        }
+        return result;
+    } catch (err) {
+        const result = {
+            url,
+            ok: false,
+            status: 0,
+            error: err.message
+        };
+        if (debug) {
+            console.log("postText", result);
+        }
+        return result;
+    }
+}
+
+export {postEmptyJson, postJson, postText};
